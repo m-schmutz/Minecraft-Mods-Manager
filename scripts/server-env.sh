a1841bf9700f190c4af41ba8bde8ce3976752286
m-schmutz <marschm131@gmail.com>
2026-02-26 18:41:45 -0800

    added script for creating server environment

---
+++ scripts/server-env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# exit on command fail
+set -e
+
+# server python environment directory
+declare -r ENV="venv-server"
+
+# ensure that the python environment exists
+if [[ ! -d "$ENV" ]]; then
+    # create server virtual environment
+    echo "Creating virtual environment"
+    python3 -m venv "$ENV"
+
+else 
+    echo "Virtual environment present"
+fi
+
+# activate virtual environment
+source ./$ENV/bin/activate
+
+# ensure pip is latest version
+echo "Upgrading pip"
+pip3 install --upgrade pip -q
+
+# update environment with packages in requirements.txt
+echo "Installing pip requirements"
+pip3 install -r lib/server-requirements.txt -q
