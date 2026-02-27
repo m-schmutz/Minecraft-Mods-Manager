065100ebfd555df15ea6b726259a830176dbba0d
m-schmutz <marschm131@gmail.com>
2026-02-26 19:43:02 -0800

    schemas for sql tables

---
+++ server/database/schemas.py
@@ -0,0 +1,43 @@
+from enum import StrEnum
+
+
+############ Mods Table ############
+
+
+class ModsTable(StrEnum):
+    '''\'Mods\' Table Information'''
+    TABLE_NAME = 'Mods'
+    ID = 'id'
+    DISPLAY_NAME = 'display_name'
+    DESCRIPTION = 'description'
+    VERSION = 'version'
+    MOD_FILE = 'mod_file'
+    FILEHASH = 'filehash'
+    LINK = 'link'
+    MOD_TYPE = 'mod_type'
+    MOD_ROLE = 'mod_role'
+
+
+class TypeValues(StrEnum):
+    '''Allowed values for the MOD_TYPE column in the \'Mods\' table'''
+    FEATURE = 'Feature'
+    LIBRARY = 'Library'
+
+
+class RoleValues(StrEnum):
+    '''Allowed values for the MOD_ROLE column in the \'Mods\' table'''
+    SERVER = 'Server'
+    CLIENT = 'Client'
+    BOTH = 'Client/Server'
+
+
+############ Dependancies Table ############
+
+
+class DepsTable(StrEnum):
+    '''\'Dependancies\' Table Information'''
+    TABLE_NAME = 'Dependancies'
+    ID = 'id'
+    MOD_ID = 'mod_id'
+    DEP_ID = 'dep_id'
+
