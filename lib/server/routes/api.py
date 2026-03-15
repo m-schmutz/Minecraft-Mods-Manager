2e496e444695891fad7fbfb70c10db0741b4694c
m-schmutz <marschm131@gmail.com>
2026-03-14 22:29:36 -0700

    added way to upload single or multiple mods to the database as well as a way for client to check mods

--- lib/server/routes/api.py
+++ lib/server/routes/api.py
@@ -3,6 +3,10 @@
 
 from flask import Blueprint, jsonify, send_file, request
 from werkzeug.utils import secure_filename
+from zipfile import ZipFile, is_zipfile
+from json import load
+from shutil import copyfileobj
+from os import remove
 
 
 #################################################################
@@ -14,13 +18,23 @@ from lib.server.database import DBConnection, ModsTable, DepsTable
 #################################################################
 # Local Imports
 
-from .utils import get_mod_filepath
+from .utils import get_mod_filepath, check_file_name, verify_jar_file
 
 
 #################################################################
 # Blueprint Object 
 
 api_bp = Blueprint('api', __name__)
+
+
+#################################################################
+# Constants
+
+JAR_EXTENSION = '.jar'
+
+JSON_EXTENSION = '.json'
+
+ZIP_EXTENSION = '.zip'
 
 
 #################################################################
@@ -62,8 +76,8 @@ def get_mod_list():
     return jsonify(modList)
 
 
-@api_bp.route('/info/mod/<modId>', methods=['GET'])
-def get_mod_info(modId: int):
+@api_bp.route('/info/mod/<id>', methods=['GET'])
+def get_mod_info(id: str):
     '''
     Return mod from database given the id of that mod
 
@@ -85,11 +99,11 @@ def get_mod_info(modId: int):
 
     # attempt to convert URL parameter to an int
     try:
-        modId = int(modId)
+        modId = int(id)
 
     # return error if the parameter cannot be converted to an int
     except ValueError:
-        return jsonify({'error': f'\'{modId}\' is not a valid integer'})
+        return jsonify({'error': f'\'{id}\' is not a valid integer'})
 
     # open connection to database
     with DBConnection() as db:
@@ -163,7 +177,9 @@ def check_client_mods():
 
 @api_bp.route('/download/mod/<filename>', methods=['GET'])
 def send_mod_file(filename: str):
-
+    '''
+    Return mod file for download given the mod filename
+    '''
     # ensure that the filename passed is safe
     filename = secure_filename(filename)
     
@@ -182,3 +198,134 @@ def send_mod_file(filename: str):
 
     # send the file that is located using the derived path
     return send_file(filepath, as_attachment=True)
+
+
+@api_bp.route('/download/mods', methods=['POST'])
+def send_mod_files():
+    return jsonify({'error': 'not implemented yet'})
+
+
+#################################################################
+# API Admin Routes
+
+@api_bp.route('/admin/upload/mod', methods=['POST'])
+def upload_mod():
+    '''
+    Upload single mod file to the server
+
+    Request must send the following data:
+    
+    - a file mapped to the key :\'mod-file\'
+
+    - a json dictionary with the following keys defined: 
+
+        - name: Display name of the mod file
+        - description: Description of what the mod does
+        - version: Display name for the version of the mod
+        - link: Link to the curseforge page for the mod
+        - type: The mod type (either 'Feature' or 'Library') 
+        - role: Where the mod needs to be present (either 'Client/Server', 'Server', 'Client')
+    '''
+    try: 
+        # get filestorage from form
+        fileStorage = request.files.get('mod-file')
+
+        # ensure that file was found
+        if not fileStorage:
+            raise ValueError('mod file must be mapped to key \'mod-file\'')
+
+        # check that filename is valid
+        safeFilename = check_file_name(fileStorage.filename, JAR_EXTENSION)
+
+        # calculate hash
+        filehash = verify_jar_file(fileStorage.stream, safeFilename)
+
+        # put insert parameters into tuple
+        params = (
+            request.form.get(ModsTable.NAME),
+            request.form.get(ModsTable.DESCRIPTION),
+            request.form.get(ModsTable.VERSION),
+            safeFilename,
+            filehash,
+            request.form.get(ModsTable.LINK),
+            request.form.get(ModsTable.TYPE),
+            request.form.get(ModsTable.ROLE)
+        )
+
+        # insert mod into the database
+        with DBConnection() as db:
+            db.insert_mod(params)
+
+        # save the mod file to either the server mods directory or the client mods directory
+        savePath = get_mod_filepath(safeFilename, request.form.get(ModsTable.ROLE))
+        fileStorage.save(savePath)
+
+        # return success
+        return jsonify({'success': f'{safeFilename} uploaded to database'})
+    
+    # return error
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+@api_bp.route('/admin/upload/mods', methods=['POST'])
+def upload_mods():
+    try: 
+        zipFile = request.files.get('mods-zip')
+        manifestFile = request.files.get('manifest')
+
+        if not zipFile:
+            raise ValueError('zip file of mods must be mapped to key \'mods-zip\'')
+
+        if not manifestFile:
+            raise ValueError('manifest json file must be mapped to key \'manifest\'')
+
+        manifest: dict[str, dict[str, str]] = load(manifestFile.stream)
+
+        savepaths = list()
+
+        with ZipFile(zipFile.stream) as z:
+            if not set(manifest.keys()) == set(z.namelist()):
+                raise ValueError('filenames in manifest file do not match filenames in zip file')
+            
+            with DBConnection() as db:
+                for zFile in z.infolist():
+
+                    metadata = manifest[zFile.filename]
+                
+                for rawFilename, metadata in manifest.items():
+                    with z.open(rawFilename) as zf:
+
+                        safeFilename = check_file_name(rawFilename, JAR_EXTENSION)
+
+                        filehash = verify_jar_file(zf, safeFilename)
+
+                        params = (
+                            metadata.get(ModsTable.NAME),
+                            metadata.get(ModsTable.DESCRIPTION),
+                            metadata.get(ModsTable.VERSION),
+                            safeFilename,
+                            filehash,
+                            metadata.get(ModsTable.LINK),
+                            metadata.get(ModsTable.TYPE),
+                            metadata.get(ModsTable.ROLE)
+                        )
+
+                        db.insert_mod(params)
+
+                        savePath = get_mod_filepath(safeFilename, metadata.get(ModsTable.ROLE))
+
+                        with open(savePath, 'wb') as f:
+                            copyfileobj(zf, f)
+                        
+                        savepaths.append(savePath)
+
+        return jsonify({'success': f'added {len(savepaths)} mods'})
+
+
+    except Exception as e:
+
+        for path in savepaths:
+            remove(path)
+
+        return jsonify({'error': f'{type(e)}: {str(e)}'})
