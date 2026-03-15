82d1010fb730852878fc06308340614f0d33d15b
m-schmutz <marschm131@gmail.com>
2026-03-14 22:30:23 -0700

    reformatted file

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
+#################################################################
+# Server Imports 
+
 from lib.server import create_app
 
 
+#################################################################
+# Flask app
+
+app = create_app()
+
+
+#################################################################
+# Main Function
+
 if __name__ == '__main__':
-    app = create_app()
+    # run flask app in debug mode
     app.run(debug=True)
