2db1541c729b56bc30bfdd095e0be9edb247ae00
m-schmutz <marschm131@gmail.com>
2026-03-22 12:17:10 -0700

    added download example

--- examples/download_mod.py
+++ examples/download_mod.py
@@ -5,9 +5,14 @@ from requests import get
 
 modFilename = 'travelersbackpack-neoforge-1.21.1-10.1.32.jar'
 
+downloadUrl = f'http://localhost:5000/api/download/mod/{modFilename}'
+
 
 if __name__ == '__main__':
-    resp = get(f'http://localhost:5000/api/download/mod/{modFilename}', stream=True)
-    
+    with get(downloadUrl, stream=True) as r:
+        r.raise_for_status()
 
-    print(f'{resp.headers["Content-Disposition"]}')
+        with open(modFilename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
