6dc3e286e54b04717e570005e9156e94d057b71b
m-schmutz <marschm131@gmail.com>
2026-03-24 15:00:36 -0700

    changed jar to import file and removed toml file

--- jar.py
+++ jar.py
@@ -1,15 +1,32 @@
 #!./venv-server/bin/python3
 
-
+from zipfile import ZipFile
 from tomlkit import loads, dumps
-
-
-
 
 
 jarPath = '/home/msch/Projects/Minecraft-Mods-Manager/examples/sample_files/alexsmobs-1.22.16.jar'
 
 tomlPath = '/home/msch/Projects/Minecraft-Mods-Manager/examples/sample_files/neoforge.mods.toml'
+
+
+
+
+def get_metadata(jarPath: str) -> tuple|None:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 with open(tomlPath, 'r') as f:
